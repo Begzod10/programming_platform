034a6176eaca796037430ba62b5f95328d50a206
ikromovvv <isardor859@gmail.com>
2026-04-01 12:45:18 +0300

    yangilanish

--- app/config.py
+++ app/config.py
@@ -22,7 +22,6 @@ class Settings(BaseSettings):
     APP_VERSION: str = "1.0.0"
 
     # Grok AI
-    GROK_API_KEY: str = "xai-sPpQKNuTEpRTv4FslxMMXloZV571Bh097H9DwAm0VVSeIrc1VtsSa3LnmOY0eBxwFzoklAoI3FghI5Mx"
     GROK_API_URL: str = "https://api.x.ai/v1/chat/completions"
     GROK_MODEL: str = "grok-3"
 
